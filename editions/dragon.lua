87ef262f96c4d8f930d5449ec4b454f530c5e7b7
kai d. gonzalez <gkai70264@gmail.com>
2024-01-17 22:00:13 -0500

    update the dragon C/C++ suite

--- editions/dragon.lua
+++ editions/dragon.lua
@@ -1,7 +1,15 @@
 --[[ Dragon - supports Lua, C/C++, Python, and Zig (via ZLS) ]]
 
+-- `enabled_languages`      - the languages that are supported by the edition
+-- `extra_extensions`       - the extensions that are installed with the edition
+-- `enabled_theme`          - leave this NIL if you initialise the extensions/themes with .hooks()
+-- `hooks`                  - all hooks that are meant to be run (primarily after setups)
+--
 return {
   enabled_languages = { "clangd", "jedi_language_server", "zls" },
   extra_extensions  = { "navarasu/onedark.nvim" },
-  enabled_theme     = "onedark",
+  enabled_theme     = nil,
+  hooks = function()
+    require("onedark").load();
+  end
 }
